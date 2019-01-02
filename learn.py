08c4cfc2e0f871a2fb0bf6c4063823c2ce239d0d
caslabs <casourcer@gmail.com>
2019-01-01 23:51:00 -1000

    env working

---
+++ learn.py
@@ -0,0 +1,19 @@
+import gym
+from gym import spaces
+space = spaces.Discrete(8)
+env = gym.make('CartPole-v0')
+'''
+for i_episode in range(20):
+    observation = env.reset()
+    for t in range(100):
+        env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
+'''
+
+print(env.observation_space.high)
+print(env.observation_space.low)
